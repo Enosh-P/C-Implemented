f51fc334d39264419062582b9631662fd2284f4d
Enosh <enoshponraj@gmail.com>
2025-12-17 21:37:45 +0100

    Add uninitialized

--- Basics/Printer.cpp
+++ Basics/Printer.cpp
@@ -4,7 +4,13 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello Enosh!"<<endl;
-	cout << "Oi";
+	cout << "Hello Enosh!"<<endl; // Flush instantly
+	cout << "Oi" << '\n'; // Flush periodically as normal and recommended
+	int x {};
+	cin >> x ;
+	char y {};
+	cin >> y;
+	char z {};
+	cout << "Output " << x << " Now Y " << y << " Now Z "<< z <<'\n';
 	return 0;
 }
